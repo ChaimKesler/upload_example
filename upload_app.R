3fd98fc8132a5b1162b6a047686d624a09d5f4bd
Anthony Kesler <anthony.kesler@aya.yale.edu>
2018-04-17 20:02:36 -0400

    Add file upload capture button

--- upload_app.R
+++ upload_app.R
@@ -15,6 +15,15 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       #Input file
+      fileInput("file1", "Choose CSV File",
+                accept = c(
+                 "text/csv",
+                 "text/comma-separated-values,text/plain",
+                 ".csv",
+                 ".txt")
+      ),
+      tags$hr(),
+      checkboxInput("header", "Header: Check if first row is header (default)", TRUE)
     ),
     # Main Panel for Instructions, Review, and Upload
     mainPanel(
@@ -25,6 +34,12 @@ ui <- fluidPage(
 # 
 server <- function(input, output) {
   # Function to Check if File Has Been Loaded
+    output$upload_status <- renderText({
+      inFile <- input$file1
+      if (is.null(inFile))
+        return("Please select data for upload")
+      "Please review your data before uploading"
+    })
   # Upload first five rows of data for preview
   # Function Used to Upload Data
   # Insert data into database
